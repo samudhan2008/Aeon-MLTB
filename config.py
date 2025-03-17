95c4d3102e97da84e81a32de80cc2025ec11c051
samudhan2008 <159536401+samudhan2008@users.noreply.github.com>
2025-03-17 17:24:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,107 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "7088416796:AAGjGk1y-rnOqI9E6cQBdb5L6Z6taBZl0zY"
+OWNER_ID = '2107729544'
+TELEGRAM_API = '27425401'
+TELEGRAM_HASH = "36150e358dd8bc2040dc8decd5250bcd"
+
+# SEMI-REQUIRED, WE SUGGEST TO FILL IT FROM MONGODB
+DATABASE_URL = "mongodb+srv://scleechadp:scleech08@scleech.gzwjf.mongodb.net/?retryWrites=true&w=majority&appName=scleech"
+
+# OPTIONAL CONFIG
+USER_SESSION_STRING = ""
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+CMD_SUFFIX = "2"
+AUTHORIZED_CHATS = "-1002433401958"
+SUDO_USERS = ""
+DEFAULT_UPLOAD = "rc"
+FILELION_API = ""
+STREAMWISH_API = ""
+EXTENSION_FILTER = ""
+INCOMPLETE_TASK_NOTIFIER = True
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = False
+NAME_SUBSTITUTE = "www.1TamilBlasters.men - | www.1TamilMV.ms - |"
+FFMPEG_CMDS = {}
+
+# INKYPINKY
+DELETE_LINKS = False
+FSUB_IDS = ""
+TOKEN_TIMEOUT = 0
+PAID_CHANNEL_ID = 0
+PAID_CHANNEL_LINK = ""
+SET_COMMANDS = True
+METADATA_KEY = "SC"
+WATERMARK_KEY = ""
+LOG_CHAT_ID = '-1002105006808'
+LEECH_FILENAME_CAPTION = ""
+
+# GDrive Tools
+GDRIVE_ID = ""
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = False
+INDEX_URL = ""
+
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+
+# Mega credentials
+MEGA_EMAIL = ""
+MEGA_PASSWORD = ""
+
+# Update
+UPSTREAM_REPO = "https://github.com/AeonOrg/Aeon-MLTB"
+UPSTREAM_BRANCH = "main"
+
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = True
+MEDIA_GROUP = False
+USER_TRANSMISSION = False
+MIXED_LEECH = False
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = "-1002208337755"
+THUMBNAIL_LAYOUT = ""
+
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = ""
+BASE_URL_PORT = 80
+WEB_PINCODE = False
+
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = ""
+RSS_SIZE_LIMIT = 0
+
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = 0
+SEARCH_PLUGINS = [
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+    "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+    "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+    "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+]
